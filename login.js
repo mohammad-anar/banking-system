64b861d1dad614e901d1458b35ad43501aa6d00e
Mohammad Anar <ai.anarul4232@gmail.com>
2023-09-18 05:42:06 +0600

    fixed file path for log in

--- login.js
+++ login.js
@@ -1,15 +1,17 @@
-// console.log('script connected');
-// step 1  add eventlistener on button
-// step 2  get input feild value that will be checked
-// step 3 checked input value 
-// step 4  if condition full fill go bank page
+/* 
+    step 1  add eventlistener on button
+    step 2  get input feild value that will be checked
+    step 3 checked input value 
+    step 4  if condition full fill go bank page
+
+*/
 
 document.getElementById('submut-btn').addEventListener('click', () => {
     const email = document.getElementById('input-email').value;    
     const password = document.getElementById('input-password').value;
 
     if (email === 'anarul@gmail.com' && password === 'anarul00') {
-        location = 'http://127.0.0.1:5500/main/bank.html';
+        location = './main/bank.html';
     }else{
         alert('Email or password not match');
     }
